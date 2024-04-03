7fb96fea5956179df8d5c9e6dab78d24c3dfd476
peterc2609 <peter.cashen@gmail.com>
2024-04-03 09:44:05 +0100

    Update Check-EventLogSize.ps1

--- Check-EventLogSize.ps1
+++ Check-EventLogSize.ps1
@@ -1,14 +1,29 @@
-$registryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Windows PowerShell"
-$valueName = "MaxSize"
-$correctValue = 1073741824
+$registrySettings = @(
+    @{Path = "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Windows PowerShell"; Name = "MaxSize"; Value = 1073741824},
+    @{Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Powershell\Operational"; Name = "MaxSize"; Value = 1073741824},
+    @{Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-TaskScheduler\Operational"; Name = "MaxSize"; Value = 274066954}
+)
 
-$value = Get-ItemPropertyValue -Path $registryPath -Name $valueName -ErrorAction SilentlyContinue
+$remediationRequired = $false
 
-if ($value -ne $correctValue) {
-    Write-Host "Remediation is required."
+foreach ($setting in $registrySettings) {
+    if (Test-Path $setting.Path) {
+        $currentValue = Get-ItemPropertyValue -Path $setting.Path -Name $setting.Name -ErrorAction SilentlyContinue
+        if ($currentValue -ne $setting.Value) {
+            Write-Host "Remediation required for $($setting.Path)"
+            $remediationRequired = $true
+        }
+    }
+    else {
+        Write-Host "Path $($setting.Path) does not exist. Remediation required."
+        $remediationRequired = $true
+    }
+}
+
+if ($remediationRequired) {
     exit 1
 }
 else {
-    Write-Host "No remediation required."
+    Write-Host "All settings are correct. No remediation required."
     exit 0
 }
