804badc17be2f11eeba7f5ddb35b1b7438de41eb
peterc2609 <peter.cashen@gmail.com>
2024-04-03 09:44:37 +0100

    Update Remediate-EventLogSize.ps1

--- Remediate-EventLogSize.ps1
+++ Remediate-EventLogSize.ps1
@@ -1,13 +1,17 @@
-$registryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Windows PowerShell"
-$valueName = "MaxSize"
-$desiredValue = 1073741824
+$registrySettings = @(
+    @{Path = "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Windows PowerShell"; Name = "MaxSize"; Value = 1073741824},
+    @{Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Powershell\Operational"; Name = "MaxSize"; Value = 1073741824},
+    @{Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-TaskScheduler\Operational"; Name = "MaxSize"; Value = 274066954}
+)
 
-# Check if the key exists; if not, create it
-if (-not (Test-Path $registryPath)) {
-    New-Item -Path $registryPath -Force | Out-Null
+foreach ($setting in $registrySettings) {
+    # Check if the key exists; if not, create it and its parents as needed
+    if (-not (Test-Path $setting.Path)) {
+        $null = New-Item -Path $setting.Path -Force
+    }
+
+    # Set the value
+    Set-ItemProperty -Path $setting.Path -Name $setting.Name -Value $setting.Value
+
+    Write-Host "Set $($setting.Name) to $($setting.Value) in $($setting.Path)"
 }
-
-# Set the value
-Set-ItemProperty -Path $registryPath -Name $valueName -Value $desiredValue
-
-Write-Host "MaxSize has been set to $desiredValue."
