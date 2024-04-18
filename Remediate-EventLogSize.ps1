d5fc806e33919afcf14e437ee6ba13f7fc37406f
peterc2609 <peter.cashen@gmail.com>
2024-04-18 15:51:13 +0100

    Update Remediate-EventLogSize.ps1

--- Remediate-EventLogSize.ps1
+++ Remediate-EventLogSize.ps1
@@ -5,10 +5,16 @@ $registrySettings = @(
     @{Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-AppLocker/EXE and DLL"; Name = "MaxSize"; Value = 104857600},
     @{Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-AppLocker/MSI and Script"; Name = "MaxSize"; Value = 104857600},
     @{Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-AppLocker/Packaged app-Deployment"; Name = "MaxSize"; Value = 104857600},
-    @{Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-AppLocker/Packaged app-Execution"; Name = "MaxSize"; Value = 104857600}   
+    @{Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-AppLocker/Packaged app-Execution"; Name = "MaxSize"; Value = 104857600}
 )
 
 $logFile = "C:\ProgramData\Microsoft\IntuneApps\EventLogSize\remediation_log.txt"
+$logDir = Split-Path -Path $logFile
+
+# Ensure the log directory exists
+if (-Not (Test-Path -Path $logDir)) {
+    New-Item -Path $logDir -ItemType Directory -Force
+}
 
 foreach ($setting in $registrySettings) {
     if (Test-Path $setting.Path) {
@@ -22,8 +28,7 @@ foreach ($setting in $registrySettings) {
             New-ItemProperty -Path $setting.Path -Name $setting.Name -Value $setting.Value -PropertyType DWord -Force
             Add-Content -Path $logFile -Value "Created $($setting.Name) with value $($setting.Value) at $($setting.Path) because it did not exist."
         }
-    }
-    else {
+    } else {
         New-Item -Path $setting.Path -Force
         New-ItemProperty -Path $setting.Path -Name $setting.Name -Value $setting.Value -PropertyType DWord -Force
         Add-Content -Path $logFile -Value "Created path $($setting.Path) and set $($setting.Name) with value $($setting.Value) because the path did not exist."
